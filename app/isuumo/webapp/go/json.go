ebfc16503fded59aaa21dd2d0331c9a5ba8f614e
Kazuki Sawada <kazuki@6715.jp>
2020-09-12 16:04:01 +0900

    use jsoniter

--- app/isuumo/webapp/go/json.go
+++ app/isuumo/webapp/go/json.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/goccy/go-json"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/labstack/echo"
 )
 
@@ -9,5 +9,5 @@ import (
 func JSON(c echo.Context, code int, i interface{}) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 	c.Response().WriteHeader(code)
-	return json.NewEncoder(c.Response()).Encode(i)
+	return jsoniter.ConfigFastest.NewEncoder(c.Response()).Encode(i)
 }
