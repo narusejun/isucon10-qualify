5bf4951fd2cffb217bc660bedfef7c60c0f4cb2e
Kazuki Sawada <kazuki@6715.jp>
2020-09-12 16:12:14 +0900

    make json-iter compatible with standard lib

--- app/isuumo/webapp/go/json.go
+++ app/isuumo/webapp/go/json.go
@@ -5,9 +5,14 @@ import (
 	"github.com/labstack/echo"
 )
 
+var json = jsoniter.Config{
+	EscapeHTML:                    false,
+	ObjectFieldMustBeSimpleString: true,
+}.Froze()
+
 // json json-iterator使用
 func JSON(c echo.Context, code int, i interface{}) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 	c.Response().WriteHeader(code)
-	return jsoniter.ConfigFastest.NewEncoder(c.Response()).Encode(i)
+	return json.NewEncoder(c.Response()).Encode(i)
 }
